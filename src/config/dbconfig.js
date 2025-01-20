6bd5297c34fe72446de772c20555f63e6d29f747
Jemimah Pico <jemimahpico@MacBook-Air-de-Jemimah.local>
2025-01-20 11:44:27 +0100

    fixed mongodb atlas connection

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async() =>{
     try {
-        let db = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@rest-api.wfrd7.mongodb.net/chatSocketCord?retryWrites=true&w=majority`,{ 
+        let db = await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wfrd7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,{ 
             useNewUrlParser: true , 
             useUnifiedTopology: true, 
             useFindAndModify: false,
             useCreateIndex: true })
         console.log(`Database connected to ${db.connection.host}`)
     } catch(err) {
-        console.log(err)
+         console.log(err)
     }
 }
 
